4a7dd540272cea256fedf4b0c20351dbdb2c31f2
black-desk <me@black-desk.cn>
2024-02-07 17:37:34 +0800

    feat: add my mosh (#15)

--- get-debs.sh
+++ get-debs.sh
@@ -9,6 +9,4 @@ mkdir -p debs
 jq -r 'keys[] as $k | "\($k) \(.[$k])"' <list.json |
 	xargs -n 2 sh -c 'gh release download --clobber -p "$1" -D debs -R "$0"'
 
-source ./pkgs/mosh.sh
-
 mv ./*.deb debs
