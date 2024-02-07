ade63da2c1189eb65e1a77cc72ce5bc80723b795
black-desk <me@black-desk.cn>
2024-02-07 15:41:29 +0800

    fix: add --clobber

--- get-debs.sh
+++ get-debs.sh
@@ -7,7 +7,7 @@ mkdir -p debs
 
 # shellcheck disable=SC2016
 jq -r 'keys[] as $k | "\($k) \(.[$k])"' <list.json |
-	xargs -n 2 sh -c 'gh release download -p "$1" -D debs -R "$0"'
+	xargs -n 2 sh -c 'gh release download --clobber -p "$1" -D debs -R "$0"'
 
 source ./pkgs/mosh.sh
 
