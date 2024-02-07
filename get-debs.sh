a804cf815fbf2513908bc67800bc7a4dd1375a5e
black-desk <me@black-desk.cn>
2024-02-07 18:11:04 +0800

    fix: remove unused code

--- get-debs.sh
+++ get-debs.sh
@@ -8,5 +8,3 @@ mkdir -p debs
 # shellcheck disable=SC2016
 jq -r 'keys[] as $k | "\($k) \(.[$k])"' <list.json |
 	xargs -n 2 sh -c 'gh release download --clobber -p "$1" -D debs -R "$0"'
-
-mv ./*.deb debs
