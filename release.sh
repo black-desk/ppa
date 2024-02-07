cd448efc6b5bf28d6b3ee00cd33370c9a213531b
black-desk <me@black-desk.cn>
2024-02-07 20:34:49 +0800

    chore: use timestamp

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ set -x
 set -e
 
 (gh release list --json tagName | jq .\[\].tagName -r | xargs -L1 gh release delete) || true
-gh release create --latest "$(date +%Y-%m-%d-%H-%M-%S)" debs/*
+gh release create --latest "$(date +%s)" debs/*
