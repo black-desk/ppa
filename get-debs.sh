8e2cc5b420d4e6208f0e03a26496d816a66a6935
black-desk <me@black-desk.cn>
2024-10-04 12:16:08 +0800

    feat: add --verbose to xargs

--- get-debs.sh
+++ get-debs.sh
@@ -7,4 +7,4 @@ mkdir -p debs
 
 # shellcheck disable=SC2016
 jq -r 'keys[] as $k | "\($k) \(.[$k])"' <list.json |
-	xargs -n 2 sh -c 'gh release download --clobber -p "$1" -D debs -R "$0"'
+	xargs --verbose -n 2 sh -c 'gh release download --clobber -p "$1" -D debs -R "$0"'
