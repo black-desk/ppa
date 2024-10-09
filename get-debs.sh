dbd876db059901309f34ad14d131be417cfd227d
black-desk <me@black-desk.cn>
2024-10-09 18:03:11 +0800

    feat: add qq

--- get-debs.sh
+++ get-debs.sh
@@ -8,3 +8,11 @@ mkdir -p debs
 # shellcheck disable=SC2016
 jq -r 'keys[] as $k | "\($k) \(.[$k])"' <list.json |
 	xargs --verbose -n 2 sh -c 'gh release download --clobber -p "$1" -D debs -R "$0"'
+
+cd debs
+
+# https://bbs.deepin.org/zh/post/279737#post_1654163
+curl -fs https://im.qq.com/rainbow/linuxQQDownload |
+	sed -nE 's/.*var params\s?=\s?(\{.*\});.*/\1/p' |
+	jq '.[] | select(type=="object") | .deb' |
+	xargs wget
