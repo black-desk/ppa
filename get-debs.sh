ba5d49b9fe6a0f93314173e53fe296c45ea2250f
black-desk <me@black-desk.cn>
2024-10-09 19:01:53 +0800

    feat: add discord

--- get-debs.sh
+++ get-debs.sh
@@ -11,6 +11,8 @@ jq -r 'keys[] as $k | "\($k) \(.[$k])"' <list.json |
 
 cd debs
 
+wget -O discord.deb "https://discord.com/api/download/stable?platform=linux&format=deb"
+
 # https://bbs.deepin.org/zh/post/279737#post_1654163
 curl -fs https://im.qq.com/rainbow/linuxQQDownload |
 	sed -nE 's/.*var params\s?=\s?(\{.*\});.*/\1/p' |
