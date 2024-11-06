62fdf59fda42b76e03365dcb35a4adf868e615db
black-desk <me@black-desk.cn>
2024-11-06 15:45:47 +0800

    feat: add wechat

--- list.sh
+++ list.sh
@@ -2,6 +2,8 @@
 
 echo "https://discord.com/api/download/stable?platform=linux&format=deb"
 
+echo "https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb"
+
 # https://bbs.deepin.org/zh/post/279737#post_1654163
 curl -fs https://im.qq.com/rainbow/linuxQQDownload |
 	sed -nE 's/.*var params\s?=\s?(\{.*\});.*/\1/p' |
